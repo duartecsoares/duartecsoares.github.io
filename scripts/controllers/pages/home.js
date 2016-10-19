a718ff1845a1aa7a613f302860c120d1fd53f672
Duarte Soares <duartecsoares@me.com>
2016-10-19 17:49:57 +0100

    data update, my links view and model added

--- scripts/controllers/pages/home.js
+++ scripts/controllers/pages/home.js
@@ -1,13 +1,21 @@
 define(["fold/controller",
+		"json!data/info.json",
 		"views/pages/home",
-		"fold/model"], function(FoldController, HomeView, FoldModel){
+		"views/sections/about-me",
+		"views/sections/my-links"], function(FoldController, infoJSON, HomeView, AboutMeView, MyLinksView){
 	
 	var HomeController = FoldController.extend({
 
 		type: "page-controller",
 
+		setup : function(){
+
+			console.log(infoJSON);
+
+		}
+
 	});
 
-	return new HomeController({ viewDetails: { constructor: HomeView, idView: "home-page-view"}});
+	return new HomeController({ viewDetails: { constructor: HomeView, idView: "home-page-view", children: [{ constructor: AboutMeView, idView: "about-me-view" }, { constructor: MyLinksView, idView: "my-links-view" }]}});
 
 });

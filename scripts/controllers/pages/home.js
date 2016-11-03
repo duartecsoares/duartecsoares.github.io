88b0adb7c394e51da8189253f0dea850fd116732
Duarte Soares <duartecsoares@me.com>
2016-11-03 17:25:11 +0000

    knowledge view added

--- scripts/controllers/pages/home.js
+++ scripts/controllers/pages/home.js
@@ -2,9 +2,10 @@ define(["fold/controller",
 		"json!data/info.json",
 		"views/pages/home",
 		"views/sections/profile",
+		"views/sections/knowledge",
 		"views/sections/journey",
 		"views/sections/work",
-		"views/sections/cta"], function(FoldController, infoJSON, HomeView, ProfileView, JourneyView, WorkView, CtaView){
+		"views/sections/cta"], function(FoldController, infoJSON, HomeView, ProfileView, KnowledgeView, JourneyView, WorkView, CtaView){
 	
 	var HomeController = FoldController.extend({
 
@@ -18,6 +19,6 @@ define(["fold/controller",
 
 	});
 
-	return new HomeController({ viewDetails: { constructor: HomeView, idView: "home-page-view", children: [{ constructor: ProfileView, idView: "profile-view" }, { constructor: JourneyView, idView: "journey-view" }, { constructor: WorkView, idView: "work-view" }, { constructor: CtaView, idView: "cta-view"}]}});
+	return new HomeController({ viewDetails: { constructor: HomeView, idView: "home-page-view", children: [{ constructor: ProfileView, idView: "profile-view" }, { constructor: KnowledgeView, idView: "knowledge-view" }, { constructor: JourneyView, idView: "journey-view" }, { constructor: WorkView, idView: "work-view" }, { constructor: CtaView, idView: "cta-view"}]}});
 
 });

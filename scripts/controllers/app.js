db3d023b2cdbd4bcd0451705e189b123f9933cf8
Duarte Soares <duartecsoares@me.com>
2016-11-04 14:45:22 +0000

    env setup and soon view added

--- scripts/controllers/app.js
+++ scripts/controllers/app.js
@@ -2,17 +2,75 @@ define(["fold/controller",
 		"router",
 		"layout",
 		"settings/app",
-		"controllers/pages/home"], function(FoldController, appRouter, layout, appConfig, homeController){
+		"controllers/pages/home",
+		"controllers/pages/soon"], function(FoldController, appRouter, layout, appConfig, homeController, soonController){
 	
-	var AppController = FoldController.extend({
+	var env = "prod", //dev or prod
+		AppController = FoldController.extend({
 
 		boot : function(){
+
+			var controller = this,
+				codeInterval,
+				bootNormal = function(){
+
+				if (codeInterval) clearInterval(codeInterval);
+
+				layout.build();
+				Backbone.router = appRouter;
+				controller.assignRoutes();
+				Backbone.history.loadUrl();
+
+				},
+				reboot = function(){
+
+					env = "dev";
+					soonController.disable();
+					layout.remove(soonController.view.idView);
+					bootNormal();
+
+			};
 			
-			layout.$dom.find("[data-view='app-loader']").remove();
-			Backbone.router = appRouter;
-			layout.build();
-			this.assignRoutes();
-			Backbone.history.loadUrl();
+			layout.$dom.find("[data-view='app-loader']").remove();			
+			
+			if(env === "dev"){
+
+				bootNormal();
+				
+			}else{
+
+				var code = [68, 69, 86],
+					step = 0;
+
+				soonController.enable();
+				layout.add([soonController.view]);
+
+				layout.$win.on("keyup", function(e){
+
+					if (env === "dev") return;
+
+					var keyCode = e.keyCode;
+
+					//d - 68, e - 69, v - 86
+					
+					if (keyCode === code[step]) {
+
+						step = step + 1;
+
+					}
+
+					if (step === 3) reboot();
+
+				});
+
+
+				codeInterval = setInterval(function(){
+
+					step = 0;
+
+				}, 6000);
+
+			}
 
 		},
 
@@ -35,7 +93,7 @@ define(["fold/controller",
 
 				});
 
-				removeableModule.disable();				
+				removeableModule.disable();		
 
 			}
 
@@ -47,6 +105,7 @@ define(["fold/controller",
 				controllersMap  = {
 					
 					"*path"	 		: homeController,
+					"dev"			: soonController,
 					""   	 		: homeController
 
 				},

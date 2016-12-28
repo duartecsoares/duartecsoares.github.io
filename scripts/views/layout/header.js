ba222ddd05f137de9856a55fe810e51a453c7ab9
Duarte Soares <duartecsoares@me.com>
2016-12-28 17:50:51 +0000

    menu mobile styles update

--- scripts/views/layout/header.js
+++ scripts/views/layout/header.js
@@ -11,28 +11,27 @@ define(["fold/view",
 
 			var view = this;				
 
-			console.log(view);
-
 			view.on("view:render", function(){
 
 				var layout = view.layout,
 					$toggle = view.$el.find("[data-action='mobile-menu-toggle']"),
+					toggleClass = "open",
 					menuMobieView;
 
 				$toggle.on("click", function(e){
-
-					console.log("toggle");
 
 					if(!menuMobieView) {
 
 						menuMobieView = new MenuMobileView({ idView: "menu-mobile-view" });
 						layout.add([menuMobieView]);
-						layout.$body.addClass("unscrollable");
+						view.$el.addClass(toggleClass);
+						layout.$body.addClass("unscrollable");						
 
 					}else{
 
 						layout.remove(menuMobieView.idView);
 						menuMobieView = null;
+						view.$el.removeClass(toggleClass);
 						layout.$body.removeClass("unscrollable");
 
 					}				

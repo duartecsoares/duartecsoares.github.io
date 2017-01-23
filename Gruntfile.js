d1b29cd06abcb27f9a3b88876e09d47f734d7aa5
Duarte Soares <duartecsoares@me.com>
2017-01-23 17:49:52 +0000

    build counter added and profile card implementation

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,27 @@ module.exports = function(grunt){
 
 	var fs 	   = require('fs'),
         task   = grunt.cli.tasks[0] || "dev",
-        jsTask = {};
+        jsTask = {},
+        setBuild = function(){
+
+		console.log("setBuild");
+
+		if (!fs.existsSync("scripts/data/build.json")) {
+
+			console.log("here new");
+
+			fs.writeFileSync("scripts/data/build.json", JSON.stringify({ build: 1 }));
+
+		}else {
+
+			var file   = JSON.parse(fs.readFileSync("scripts/data/build.json", "utf-8")),
+				build  = { build : file.build + 1 };
+
+			fs.writeFileSync("scripts/data/build.json", JSON.stringify(build));
+
+		}
+
+    }
 
     if (task === "production") {
 
@@ -28,6 +48,7 @@ module.exports = function(grunt){
 		};
 
     }
+
 
 	grunt.initConfig({
 
@@ -127,7 +148,7 @@ module.exports = function(grunt){
 			css: {
 
 		       	files: 'ui-kit/**/*.scss',
-		       	tasks: ["sass", "autoprefixer"]		
+		       	tasks: ["sass", "autoprefixer", "set-build"]		
 
 		    },
 
@@ -135,7 +156,7 @@ module.exports = function(grunt){
 
 		}
 
-	});	
+	});
 	
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks("grunt-complexity");
@@ -152,7 +173,7 @@ module.exports = function(grunt){
 
 	grunt.registerTask("report", ["complexity"]);
 	grunt.registerTask("bower-install", ["bower-install-simple"]);
-	//grunt.registerTask("default");
+	grunt.registerTask("set-build", setBuild);
 	//grunt.registerTask("release", ["requirejs"]);
 	//grunt.registerTask("default", ["sass", "watch"]);
 	//grunt.registerTask("prod", ["sass"]);
@@ -162,32 +183,32 @@ module.exports = function(grunt){
         case "dev":
 
             grunt.file.copy("scripts/app.js", "dist/js/fold-webapp-pack.js");
-            grunt.registerTask("default", "Development Task", ["sass", "autoprefixer", "watch"]);
+            grunt.registerTask("default", "Development Task", ["sass", "autoprefixer", "set-build", "watch"]);
 
             break;
 
         case "release":
 
             grunt.file.copy("scripts/app.js", "dist/js/fold-webapp-pack.js");
-            grunt.registerTask("release", "Release Task", ["sass", "autoprefixer"]);
+            grunt.registerTask("release", "Release Task", ["sass", "autoprefixer", "set-build"]);
 
         break;
 
         case "production-end":
 
-            grunt.registerTask("production-end", "Production Task", ["requirejs", "sass", "autoprefixer"]);
+            grunt.registerTask("production-end", "Production Task", ["requirejs", "sass", "autoprefixer", "set-build"]);
 
         break;
 
         case "prod":
 
-            grunt.registerTask("prod", "Production Task", ["requirejs", "sass", "autoprefixer"]);
+            grunt.registerTask("prod", "Production Task", ["requirejs", "sass", "autoprefixer", "set-build"]);
 
         break;
 
          case "production":
 
-            grunt.registerTask("production", "Production Task", ["requirejs", "sass", "watch", "autoprefixer"]);
+            grunt.registerTask("production", "Production Task", ["requirejs", "sass", "set-build", "watch", "autoprefixer"]);
 
         break;
 
--- scripts/views/sections/terminal.js
+++ scripts/views/sections/terminal.js
@@ -1,5 +1,7 @@
 define(["fold/view",
-		"text!templates/sections/terminal.html"], function(FoldView, template){
+		"text!templates/sections/terminal.html",
+		"json!data/info.json",
+		"json!data/build.json"], function(FoldView, template, dataJSON, buildJSON){
 	
 	var TerminalSectionView = FoldView.extend({
 
@@ -8,6 +10,17 @@ define(["fold/view",
 		setup: function(){
 
 			/* setup fn serves as an specific initializer */
+			this.model = {
+
+				user 	: dataJSON.user,
+				system  : {
+
+					version : dataJSON.app.version,
+					build : buildJSON.build
+
+				}
+
+			}
 
 		}
 
